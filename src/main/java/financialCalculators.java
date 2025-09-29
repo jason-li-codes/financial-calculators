95fa44609b8b04ff858c5c09ef77c04554d33961
jasonli <lijason991@gmail.com>
2025-09-29 10:14:52 -0400

    third commit

--- src/main/java/financialCalculators.java
+++ src/main/java/financialCalculators.java
@@ -15,6 +15,7 @@ public class financialCalculators {
                 (1) Mortgage calculator
                 (2) Compound interest calculator
                 (3) Annuity value calculator
+                (4) More info
                 """);
 
         int whichCalc = input.nextInt();
@@ -22,23 +23,39 @@ public class financialCalculators {
 
         switch (whichCalc) {
             case 1:
-                mortageCalc();
+                mortgageCalc(input);
                 break;
             case 2:
-                interestCalc();
+                interestCalc(input);
                 break;
             case 3:
-                annuityCalc();
+                annuityCalc(input);
                 break;
+            case 4:
+                System.out.println("""
+                        Mortgage calculator will determine how much a monthly payment for a loan would be (minus any
+                        insurance or taxes), as well as how much interest you would pay over the life of the loan.
+                        Interest calculator will determines the future value of a one-time deposit assuming compound
+                        interest.
+                        Annuity calculator will determines the present value of an ordinary annuity.
+                        To go back to main menu, rerun program.
+                        """);
             default:
                 System.out.println("Incorrect selection, please rerun program.");
         }
 
         }
 
+    public static void mortgageCalc(Scanner input) {
+
+        System.out.println("""
+                We will calculate how much a monthly payment for a loan would be (minus any insurance or taxes), as well
+                as how much interest you would pay over the life of the loan.
+                What will the principal of the mortgage be?
+                """);
+
+
+
     }
-
-
-
 
 }
