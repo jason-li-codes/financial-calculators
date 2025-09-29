af93514056625a4a8471315e73352f8060f79917
jasonli <lijason991@gmail.com>
2025-09-29 10:05:17 -0400

    second commit

--- src/main/java/financialCalculators.java
+++ src/main/java/financialCalculators.java
@@ -8,6 +8,8 @@ public class financialCalculators {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
         System.out.println("""
                 Hello, which financial calculator would you like to use?
                 (1) Mortgage calculator
@@ -15,9 +17,24 @@ public class financialCalculators {
                 (3) Annuity value calculator
                 """);
 
+        int whichCalc = input.nextInt();
+        input.nextLine();
 
+        switch (whichCalc) {
+            case 1:
+                mortageCalc();
+                break;
+            case 2:
+                interestCalc();
+                break;
+            case 3:
+                annuityCalc();
+                break;
+            default:
+                System.out.println("Incorrect selection, please rerun program.");
+        }
 
-
+        }
 
     }
 
