efb9a3006b584c9a9c4b211f15c224709435a3b5
jasonli <lijason991@gmail.com>
2025-09-29 13:42:22 -0400

    finished all Calcs

--- src/main/java/financialCalculators.java
+++ src/main/java/financialCalculators.java
@@ -44,19 +44,19 @@ public class financialCalculators {
                 System.out.println("Incorrect selection, please rerun program.");
         }
 
-        }
+    }
 
     public static void mortgageCalc(Scanner input) {
 
         System.out.println("What will the principal of the mortgage be?");
         double principal = input.nextDouble();
-        System.out.println("What will the interest rate percentage be?");
+        System.out.println("What will the annual interest rate percentage be?");
         double interestAnnual = input.nextDouble();
         System.out.println("What will the loan length  be in years?");
-        int loanLengthYears = input.nextInt();
+        double loanLengthYears = input.nextDouble();
 
         double interest = interestAnnual / 100 / 12;
-        int loanLengthMonths = loanLengthYears * 12;
+        double loanLengthMonths = loanLengthYears * 12;
 
         double monthPayment = principal * ((interest * Math.pow((1 + interest), loanLengthMonths)) /
                 (Math.pow((1 + interest), loanLengthMonths) - 1));
@@ -64,10 +64,52 @@ public class financialCalculators {
         double totalInterest = monthPayment * loanLengthMonths - principal;
 
         System.out.printf("""
-                Your expected month payments would be $.2f, for %d months.
-                Your total interest paid would be $.2f.
-                """,
+                        Your expected monthly payments would be $.2f, for %d months.
+                        Your total interest paid would be $.2f.
+                        """,
                 monthPayment, loanLengthMonths, totalInterest);
+
+    }
+
+    public static void interestCalc(Scanner input) {
+
+        System.out.println("What will the deposit be?");
+        double deposit = input.nextDouble();
+        System.out.println("What will the annual interest rate percentage be?");
+        double interestAnnualPercentage = input.nextDouble();
+        System.out.println("What will the loan length  be in years?");
+        double CDLengthYears = input.nextDouble();
+
+        double interestAnnual = interestAnnualPercentage / 100;
+
+        double futureValue = deposit * Math.pow(1 + (interestAnnual / 365), 365 * CDLengthYears);
+
+        double totalInterest = futureValue - deposit;
+
+        System.out.printf("""
+                        The future value of your deposit would be $%.2f.
+                        The total interest earned would be $.2f.
+                        """,
+                futureValue, totalInterest);
+
+    }
+
+    public static void annuityCalc(Scanner input) {
+
+        System.out.println("What is the monthly payout of the annuity?");
+        double monthlyPayout = input.nextDouble();
+        System.out.println("What is the expected monthly interest rate percentage?");
+        double monthlyInterestPercentage = input.nextDouble();
+        System.out.println("What is the length of the annuity in years?");
+        double annuityLengthYears = input.nextDouble();
+
+        double monthlyInterest = monthlyInterestPercentage / 100;
+        double annuityLengthMonths = annuityLengthYears * 12;
+
+        double presentValue = monthlyPayout *
+                ((1 - Math.pow(1 + monthlyInterest, annuityLengthMonths * -1)) / monthlyInterest);
+
+        System.out.printf("The present value of the annuity would be $%.2f.", presentValue);
 
     }
 
