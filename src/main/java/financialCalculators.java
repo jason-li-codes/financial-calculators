e7867af07329ee36fa04b2cb3bc9e9daf95098ac
jasonli <lijason991@gmail.com>
2025-09-29 09:40:53 -0400

    first commit

---
+++ src/main/java/financialCalculators.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class financialCalculators {
+
+    public static void main(String[] args) {
+
+        System.out.println("""
+                Hello, which financial calculator would you like to use?
+                (1) Mortgage calculator
+                (2) Compound interest calculator
+                (3) Annuity value calculator
+                """);
+
+
+
+
+
+    }
+
+
+
+
+}
